52e141bdac03f2dd56f78e6ec40722521305fc37
MauroMVI <mauromvi100695@gmail.com>
2020-05-16 14:14:59 +0300

    Unknown changes.

--- ship.py
+++ ship.py
@@ -3,7 +3,7 @@ import pygame
 class Ship:
     """A class to manage the ship."""
 
-    def __init__(self,ai_game):
+    def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
         self.settings = ai_game.settings
