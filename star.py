420936b4c69404b9c65d9f47a7eab019c7efc47a
MauroMVI <mauromvi100695@gmail.com>
2020-05-17 11:37:27 +0300

    Added smaller star image, and requirements for a gameover.

--- star.py
+++ star.py
@@ -7,14 +7,15 @@ class Star(Sprite):
         """Initialize the star and set its starting position."""
         super().__init__()
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
 
         # Load the star image and set its rect attribute.
-        self.image = pygame.image.load('images/star.bmp')
+        self.image = pygame.image.load('images/star_10.bmp')
         self.rect = self.image.get_rect()
 
         # Start each new star near the top left of the screen
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
 
-        # Store the alien's exact horizontal position.
-        # self.x = float(self.rect.x)
+        # Store the star's exact horizontal position.
+        self.y = float(self.rect.y)
